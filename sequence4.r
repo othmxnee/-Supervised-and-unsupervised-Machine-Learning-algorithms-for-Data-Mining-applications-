4db565eb44ecdeee3097aa1a926105342e1d5d9d
othmane <othmane@kali.kali>
2025-11-06 09:50:12 +0100

    data preprocessing

---
+++ sequence4.r
@@ -0,0 +1,21 @@
+# install.packages("datasets") 
+# install.packages("caTools") 
+#  install.packages("party")
+#  install.packages("dplyr")
+#  install.packages("magrittr") 
+
+
+
+
+#  library(datasets)
+#  library(caTools)
+#  library(party)
+#  library(dplyr)
+#  library(magrittr)
+#  library(lubridate)
+#  library(tidyverse) 
+
+# dataset <- read.csv("dataset.csv")
+
+# 
+dataset[duplicated(dataset),]
